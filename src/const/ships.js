3b76a15c4849607ceb4dc69f13b08457fddca727
juanMorenoDev <juangui9696@gmail.com>
2021-11-07 11:52:02 -0500

    fix: drowned ships counter

--- src/const/ships.js
+++ src/const/ships.js
@@ -1,32 +1,42 @@
 export default [
   {
+    id: 1,
     size: 4,
   },
   {
+    id: 2,
     size: 3,
   },
   {
+    id: 3,
     size: 3,
   },
   {
+    id: 4,
     size: 2,
   },
   {
+    id: 5,
     size: 2,
   },
   {
+    id: 6,
     size: 2,
   },
   {
+    id: 7,
     size: 1,
   },
   {
+    id: 8,
     size: 1,
   },
   {
+    id: 9,
     size: 1,
   },
   {
+    id: 10,
     size: 1,
   },
 ]
